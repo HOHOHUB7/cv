69855773220518165ddcf43675cddbe749fddab5
HOHOHUB7 <155407727+HOHOHUB7@users.noreply.github.com>
2025-05-10 20:00:59 -0300

    Update custom.js

--- custom.js
+++ custom.js
@@ -38,6 +38,19 @@ function gtag() {
 }
 gtag('js', new Date());
 gtag('config', 'AW-17070494767');
+ }else{
+     (function () {
+  var script = document.createElement('script');
+  script.src = 'https://www.googletagmanager.com/gtag/js?id=AW-17065247849';
+  script.async = true;
+  document.head.appendChild(script);
+})();
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+  dataLayer.push(arguments);
+}
+gtag('js', new Date());
+gtag('config', 'AW-17065247849');
  }
 
   function crc16(str) {
