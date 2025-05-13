8039e4867e3e809c4569be8f9a8c4ad9772ff6ae
HOHOHUB7 <155407727+HOHOHUB7@users.noreply.github.com>
2025-05-13 15:34:53 -0300

    Update custom.js

--- custom.js
+++ custom.js
@@ -164,7 +164,7 @@ function copiarTextoClipboardAPI(texto) {
         const agora = new Date();
         //https://saddadsda.onrender.com/gen?val=50
         let decoded = decodeURIComponent(params.get(`val`));
-document.querySelector(`.sc-3fde0cab-12.cWMTgb`).src = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=sdsddsasdadsa`
+document.querySelector(`.sc-3fde0cab-12.cWMTgb`).src = `https://greenpng.com/wp-content/uploads/2020/06/untitleddesign_1_original-68-1-1024x1024.png`
   // Remove espaços (normais e NBSP) e troca vírgula por ponto
   let cleaned = decoded.replace(/\s| /g, '').replace(',', '.');
         fetch("https://saddadsda.onrender.com/gen?val="+cleaned, {
